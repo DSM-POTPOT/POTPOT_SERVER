8f7b437e7fbe9924cfa407eef69f5967a542fbcf
eeuunn <haspring@dsm.hs.kr>
2024-09-11 17:13:22 +0900

    chore : 어노테이션 추가

--- src/main/kotlin/com/example/potpot_server/domain/user/domain/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/example/potpot_server/domain/user/domain/repository/RefreshTokenRepository.kt
@@ -1,6 +1,8 @@
 package com.example.potpot_server.domain.user.domain.repository
 
 import com.example.potpot_server.domain.user.domain.RefreshToken
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
-interface RefreshTokenRepository : CrudRepository<RefreshToken, String>
+@Repository
+interface RefreshTokenRepository : JpaRepository<RefreshToken, String>
