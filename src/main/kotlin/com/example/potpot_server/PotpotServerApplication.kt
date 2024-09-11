9abd13fe4ec934f21a5ea1d09aea8ed033ba478e
eeuunn <haspring@dsm.hs.kr>
2024-09-11 16:55:17 +0900

    chore

--- src/main/kotlin/com/example/potpot_server/PotpotServerApplication.kt
+++ src/main/kotlin/com/example/potpot_server/PotpotServerApplication.kt
@@ -1,8 +1,11 @@
 package com.example.potpot_server
 
+import com.example.potpot_server.global.security.jwt.TokenProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
+@EnableConfigurationProperties(TokenProperties::class)
 @SpringBootApplication
 class PotpotServerApplication
 
