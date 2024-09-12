ce645c8ec6862d155962b6b5c137e949bb8970f0
eeuunn <haspring@dsm.hs.kr>
2024-09-12 19:54:20 +0900

    fix(user) : title, text 수정

--- src/main/kotlin/com/example/potpot_server/domain/auth/service/EmailTool.kt
+++ src/main/kotlin/com/example/potpot_server/domain/auth/service/EmailTool.kt
@@ -1,21 +1,15 @@
 package com.example.potpot_server.domain.auth.service
 
 import com.example.potpot_server.domain.auth.presentation.EmailVerifyNumberRequest
-import lombok.RequiredArgsConstructor
-import lombok.extern.slf4j.Slf4j
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Lazy
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 
 // 인증번호 전송 서비스
 @Service
 class EmailTool(
-    @Lazy
     private val javaMailSender: JavaMailSender
 ) {
 
@@ -30,11 +24,13 @@ class EmailTool(
             log.error("이메일 발송 오류", e)
         }
     }
+
     private fun createEmailForm(emailVerifyNumberRequest: EmailVerifyNumberRequest): SimpleMailMessage {
         return SimpleMailMessage().apply {
             setTo(emailVerifyNumberRequest.email)
-            setSubject(emailVerifyNumberRequest.title)
-            setText(emailVerifyNumberRequest.text)
+            setSubject("유저 이메일 인증 번호")
+            setText("POTPOT 이메일 인증번호 입니다. 타인에게 공유하지 마세요")
+
         }
     }
 }
