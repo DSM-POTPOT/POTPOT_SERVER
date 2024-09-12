3481782542ebe1147060afa50c477e323ede4859
eeuunn <haspring@dsm.hs.kr>
2024-09-12 15:45:13 +0900

    feat(user) : 인증번호 Config

---
+++ src/main/kotlin/com/example/potpot_server/domain/auth/service/MailConfig.kt
@@ -0,0 +1,50 @@
+package com.example.potpot_server.domain.auth.service
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.JavaMailSenderImpl
+import java.util.*
+
+@Configuration
+class MailConfig(
+) {
+    @Value("\${spring.mail.host}")
+    private val host: String? = null
+
+    @Value("\${spring.mail.port}")
+    private val port = 0
+
+    @Value("\${spring.mail.username}")
+    private val username: String? = null
+
+    @Value("\${spring.mail.password}")
+    private val password: String? = null
+
+    @Value("\${spring.mail.properties.mail.smtp.auth}")
+    private val auth = false
+
+    @Bean
+    fun javaMailSender(): JavaMailSender{
+        val javaMailSender = JavaMailSenderImpl().apply {
+            host = "smtp.gmail.com"
+            username = "haspring@dsm.hs.kr"
+            password = "nefhzscunhjqgyga"
+            port = 587
+            defaultEncoding = "UTF-8"
+            javaMailProperties = getMailProperties()
+        }
+        return javaMailSender
+    }
+
+    @Bean
+    fun getMailProperties(): Properties {
+        return Properties().apply {
+            setProperty("mail.smtp.auth", "true")
+            setProperty("mail.smtp.host", host)
+            setProperty("mail.smtp.port", port.toString())
+            setProperty("mail.smtp.starttls.enable", "true")
+        }
+    }
+}
