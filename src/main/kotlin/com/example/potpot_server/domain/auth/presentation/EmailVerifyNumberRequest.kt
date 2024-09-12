c3dabb2c6b5c9b80e4a18fd4868454a08217a005
eeuunn <haspring@dsm.hs.kr>
2024-09-13 08:38:05 +0900

    fix(user) :add jsonproperty

--- src/main/kotlin/com/example/potpot_server/domain/auth/presentation/EmailVerifyNumberRequest.kt
+++ src/main/kotlin/com/example/potpot_server/domain/auth/presentation/EmailVerifyNumberRequest.kt
@@ -1,5 +1,10 @@
 package com.example.potpot_server.domain.auth.presentation
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.NotNull
+
 data class EmailVerifyNumberRequest(
+    @JsonProperty("email")
+    @NotNull
     val email: String,
 )
