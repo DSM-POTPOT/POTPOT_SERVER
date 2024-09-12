de16a1e34bda1066dda2d776472400b125fd1b73
eeuunn <haspring@dsm.hs.kr>
2024-09-12 15:45:45 +0900

    feat(user) : redis관련 설정

---
+++ src/main/kotlin/com/example/potpot_server/domain/auth/service/RedisTool.kt
@@ -0,0 +1,35 @@
+package com.example.potpot_server.domain.auth.service
+
+import org.slf4j.LoggerFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.ValueOperations
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import java.time.Duration
+
+@Component
+@Transactional
+class RedisTool(private val redisTemplate: RedisTemplate<String, Any>) {
+
+    private val log = LoggerFactory.getLogger(RedisTool::class.java)
+
+    fun setValues(key: String, data: String) {
+        val values: ValueOperations<String, Any> = redisTemplate.opsForValue()
+        values.set(key, data)
+    }
+
+    fun setValues(key: String, data: String, duration: Duration) {
+        val values: ValueOperations<String, Any> = redisTemplate.opsForValue()
+        values.set(key, data, duration)
+    }
+
+    @Transactional(readOnly = true)
+    fun getValues(key: String): String {
+        val values: ValueOperations<String, Any> = redisTemplate.opsForValue()
+        return values.get(key)?.toString() ?: "false"
+    }
+
+    fun checkExistsValue(value: String): Boolean {
+        return value != "false"
+    }
+}
