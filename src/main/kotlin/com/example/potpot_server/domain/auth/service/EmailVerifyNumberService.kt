954edf90dba281efc4fde63330b64541edc70278
eeuunn <haspring@dsm.hs.kr>
2024-09-16 00:09:17 +0900

    feat(user) : 인증번호 요청 서비스

--- src/main/kotlin/com/example/potpot_server/domain/auth/service/EmailVerifyNumberService.kt
+++ src/main/kotlin/com/example/potpot_server/domain/auth/service/EmailVerifyNumberService.kt
@@ -1,8 +1,9 @@
 package com.example.potpot_server.domain.auth.service
 
+import com.example.potpot_server.domain.auth.domain.repository.EmailVerifyRepository
 import com.example.potpot_server.domain.auth.exception.CustomException
-import com.example.potpot_server.domain.auth.presentation.EmailVerifyNumberRequest
-import com.example.potpot_server.domain.user.domain.repository.UserRepository
+import com.example.potpot_server.domain.auth.presentation.dto.EmailVerifyNumberRequest
+import com.example.potpot_server.global.redis.RedisTool
 import lombok.RequiredArgsConstructor
 import lombok.extern.slf4j.Slf4j
 import org.slf4j.LoggerFactory
@@ -21,9 +22,9 @@ import java.util.*
 @Transactional
 @RequiredArgsConstructor
 class EmailVerifyNumberService(
-    private val userRepository: UserRepository,
     private val redisTool: RedisTool,
-    private val javaMailSender: JavaMailSender
+    private val javaMailSender: JavaMailSender,
+    private val emailVerifyRepository: EmailVerifyRepository
 ) {
     private val log = LoggerFactory.getLogger(EmailVerifyNumberService::class.java)
 
@@ -35,31 +36,19 @@ class EmailVerifyNumberService(
         val checkNum = random.nextInt(888888) + 111111
         return checkNum.toString()
     }
+    val random = makeRandomNumber()
 
-    fun sendCodeToEmail(emailVerifyNumberRequest: EmailVerifyNumberRequest) {
-        // 이메일 중복 검사
-        if (userRepository.existsByMail(emailVerifyNumberRequest.email)) {
-            throw CustomException(HttpStatus.CONFLICT, "중복 된 이메일 입니다.")
+    private fun createEmailForm(request: EmailVerifyNumberRequest): SimpleMailMessage {
+        return SimpleMailMessage().apply {
+            setTo(request.email)
+            setSubject("유저 이메일 인증 번호")
+            setText("POTPOT 이메일 인증번호 입니다 : ${random} 타인에게 공유하지 마세요")
         }
-
-        // 인증코드 생성, 저장 및 이메일 전송
-        // 이메일 인증 요청 시 인증 번호 Redis에 저장
-        redisTool.setValues(
-            emailVerifyNumberRequest.email, makeRandomNumber(),
-            Duration.ofMillis(authCodeExpirationMillis)
-        )
-        sendEmail(emailVerifyNumberRequest)
     }
-
-    fun verifyCode(email: String, authCode: String): Boolean {
-        val redisAuthCode = redisTool.getValues(authCode + email)
-        return redisTool.checkExistsValue(redisAuthCode) && redisAuthCode == authCode
-    }
-
 
     // 인증번호 전송 서비스
-    fun sendEmail(emailVerifyNumberRequest: EmailVerifyNumberRequest) {
-        val emailForm = createEmailForm(emailVerifyNumberRequest)
+    fun sendEmail(request: EmailVerifyNumberRequest) {
+        val emailForm = createEmailForm(request)
         try {
             javaMailSender.send(emailForm)
             log.info("이메일 발송 성공")
@@ -68,12 +57,17 @@ class EmailVerifyNumberService(
         }
     }
 
-    private fun createEmailForm(emailVerifyNumberRequest: EmailVerifyNumberRequest): SimpleMailMessage {
-        return SimpleMailMessage().apply {
-            setTo(emailVerifyNumberRequest.email)
-            setSubject("유저 이메일 인증 번호")
-            setText("POTPOT 이메일 인증번호 입니다 : ${makeRandomNumber()} 타인에게 공유하지 마세요")
+    fun sendCodeToEmail(emailVerifyNumberRequest: EmailVerifyNumberRequest) {
+        // 이메일 중복 검사
+        if (emailVerifyRepository.existsByEmail(emailVerifyNumberRequest.email)) {
+            throw CustomException(HttpStatus.CONFLICT, "중복 된 이메일 입니다.")
         }
+        // 인증코드 생성, 저장 및 이메일 전송
+        // 이메일 인증 요청 시 인증 번호 Redis에 저장
+        redisTool.setValues(
+            emailVerifyNumberRequest.email, random,
+            Duration.ofMillis(authCodeExpirationMillis)
+        )
+        sendEmail(emailVerifyNumberRequest)
     }
-
 }
