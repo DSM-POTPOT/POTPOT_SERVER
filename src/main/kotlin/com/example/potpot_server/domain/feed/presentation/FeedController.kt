8185e4df27c7688c48f5fb0959ab2984b2f98e46
soohyeon <meltapplee@gmail.com>
2024-10-13 23:50:37 +0900

    feat(feed) : feed 삭제 api

--- src/main/kotlin/com/example/potpot_server/domain/feed/presentation/FeedController.kt
+++ src/main/kotlin/com/example/potpot_server/domain/feed/presentation/FeedController.kt
@@ -2,10 +2,13 @@ package com.example.potpot_server.domain.feed.presentation
 
 import com.example.potpot_server.domain.feed.presentation.dto.request.CreateRequest
 import com.example.potpot_server.domain.feed.service.CreateFeedService
+import com.example.potpot_server.domain.feed.service.DeleteFeedService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -14,7 +17,8 @@ import org.springframework.web.multipart.MultipartFile
 @RestController
 @RequestMapping("/feed")
 class FeedController(
-    private val createFeedService: CreateFeedService
+    private val createFeedService: CreateFeedService,
+    private val deleteFeedService: DeleteFeedService
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -24,4 +28,9 @@ class FeedController(
         file: MultipartFile?,
         @RequestPart(name = "request") @Valid request: CreateRequest
     ) = createFeedService.execute(file, request)
+
+    @ResponseStatus
+    @DeleteMapping
+    fun delete(@RequestParam(name = "id") id: Long) =
+        deleteFeedService.execute(id)
 }
