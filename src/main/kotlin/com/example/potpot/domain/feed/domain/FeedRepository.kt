d95b5e51a5a66fca8e28a10198c97b95dd38c23b
soohyeon <meltapplee@gmail.com>
2024-10-24 12:24:24 +0900

    feat(feed) : feed 전체/카테고리별 조회 api

--- src/main/kotlin/com/example/potpot/domain/feed/domain/FeedRepository.kt
+++ src/main/kotlin/com/example/potpot/domain/feed/domain/FeedRepository.kt
@@ -1,5 +1,8 @@
 package com.example.potpot.domain.feed.domain
 
+import com.example.potpot.domain.feed.enum.Category
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface FeedRepository : JpaRepository<Feed, Long>
+interface FeedRepository : JpaRepository<Feed, Long> {
+    fun findAllByCategory(category: Category): List<Feed>
+}
