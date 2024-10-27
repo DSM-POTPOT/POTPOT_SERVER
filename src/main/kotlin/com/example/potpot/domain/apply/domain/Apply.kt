1eb888767222ad6c8164a53d4530cd37abd2ede2
soohyeon <meltapplee@gmail.com>
2024-10-27 23:18:19 +0900

    feat(apply) : Apply entity 추가

---
+++ src/main/kotlin/com/example/potpot/domain/apply/domain/Apply.kt
@@ -0,0 +1,37 @@
+package com.example.potpot.domain.apply.domain
+
+import com.example.potpot.domain.feed.domain.Feed
+import com.example.potpot.domain.user.domain.User
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+@Entity(name = "tbl_apply")
+data class Apply(
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    val id: Long = 0,
+
+    @Column(name = "is_ok", nullable = false)
+    val isOK: Boolean = false,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val feed: Feed
+) {
+    constructor() : this(
+        id = 0,
+        isOK = false,
+        feed = Feed(),
+        user = User()
+    )
+}
---
+++ src/main/kotlin/com/example/potpot/domain/apply/domain/ApplyRepository.kt
@@ -0,0 +1,5 @@
+package com.example.potpot.domain.apply.domain
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ApplyRepository : JpaRepository<Apply, Long>
