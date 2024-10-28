7f48fc05bedf7f33b3da6fdba400559ac389bcce
soohyeon <meltapplee@gmail.com>
2024-10-28 19:45:27 +0900

    feat(apply) : apply 삭제 api

--- src/main/kotlin/com/example/potpot/domain/apply/presentation/ApplyController.kt
+++ src/main/kotlin/com/example/potpot/domain/apply/presentation/ApplyController.kt
@@ -2,19 +2,27 @@ package com.example.potpot.domain.apply.presentation
 
 import com.example.potpot.domain.apply.presentation.dto.request.ApplyRequest
 import com.example.potpot.domain.apply.sevice.ApplyService
+import com.example.potpot.domain.apply.sevice.DeleteApplyService
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/apply")
 class ApplyController(
-    private val applyService: ApplyService
+    private val applyService: ApplyService,
+    private val deleteApplyService: DeleteApplyService
 ) {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun create(@RequestBody request: ApplyRequest) = applyService.execute(request)
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping
+    fun delete(@RequestParam(name = "id")id: Long) = deleteApplyService.execute(id)
 }
