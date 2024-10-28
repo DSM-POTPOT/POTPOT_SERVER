cf57946a72fffe5cea7f4ec9fc8ce060649ad2f5
eeuunn <haspring@dsm.hs.kr>
2024-10-28 20:17:17 +0900

    feat(feed) : dto 추가

--- src/main/kotlin/com/example/potpot/domain/feed/presentation/dto/request/FeedRequest.kt
+++ src/main/kotlin/com/example/potpot/domain/feed/presentation/dto/request/FeedRequest.kt
@@ -2,6 +2,7 @@ package com.example.potpot.domain.feed.presentation.dto.request
 
 import com.example.potpot.domain.feed.enum.Category
 import jakarta.validation.constraints.Size
+import java.beans.ConstructorProperties
 import java.time.LocalDate
 
 data class FeedRequest(
