93fb3918335725d85ce7acd8981f49eb57a436e0
eeuunn <haspring@dsm.hs.kr>
2024-10-28 22:01:28 +0900

    feat(feed) : 게시물 상세조회 response 추가

---
+++ src/main/kotlin/com/example/potpot/domain/feed/presentation/dto/response/FeedDetailsResponse.kt
@@ -0,0 +1,15 @@
+package com.example.potpot.domain.feed.presentation.dto.response
+
+import com.example.potpot.domain.feed.enum.Category
+
+data class FeedDetailsResponse(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val date: String,
+    val category: Category,
+    val image: String?,
+    val isOK: Boolean,
+    val userName: String,
+    val comment: String
+)
