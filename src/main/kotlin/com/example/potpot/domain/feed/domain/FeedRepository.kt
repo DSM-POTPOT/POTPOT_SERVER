cbc6a1ab7eb7654a416998e4b9b0fa710b104967
eeuunn <haspring@dsm.hs.kr>
2024-10-28 20:17:03 +0900

    fix(feed) : feedRepository 수정

--- src/main/kotlin/com/example/potpot/domain/feed/domain/FeedRepository.kt
+++ src/main/kotlin/com/example/potpot/domain/feed/domain/FeedRepository.kt
@@ -2,7 +2,10 @@ package com.example.potpot.domain.feed.domain
 
 import com.example.potpot.domain.feed.enum.Category
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface FeedRepository : JpaRepository<Feed, Long> {
     fun findAllByCategory(category: Category): List<Feed>
+
+    override fun findById(id: Long): Optional<Feed>
 }
