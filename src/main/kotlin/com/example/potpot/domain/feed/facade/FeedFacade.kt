83e20174a3a561b6a0bae26ec17e0d06ddbe09a9
eeuunn <haspring@dsm.hs.kr>
2024-10-28 20:16:40 +0900

    featIfeed) : facade 패턴 추가

---
+++ src/main/kotlin/com/example/potpot/domain/feed/facade/FeedFacade.kt
@@ -0,0 +1,16 @@
+package com.example.potpot.domain.feed.facade
+
+import com.example.potpot.domain.feed.domain.Feed
+import com.example.potpot.domain.feed.domain.FeedRepository
+import org.springframework.stereotype.Component
+import java.util.Optional
+import javax.swing.text.html.Option
+
+@Component
+class FeedFacade(
+    private val feedRepository: FeedRepository
+) {
+    fun getCurrentFeed(id: Long): Optional<Feed>{
+        return feedRepository.findById(id)
+    }
+}
