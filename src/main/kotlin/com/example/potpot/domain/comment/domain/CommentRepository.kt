19dc3ad6638b5ebf36b5a63f5b45d46bd7d350f9
eeuunn <haspring@dsm.hs.kr>
2024-10-30 16:52:15 +0900

    chore :  import 삭제

--- src/main/kotlin/com/example/potpot/domain/comment/domain/CommentRepository.kt
+++ src/main/kotlin/com/example/potpot/domain/comment/domain/CommentRepository.kt
@@ -1,6 +1,5 @@
 package com.example.potpot.domain.comment.domain
 
-import com.example.potpot.domain.comment.domain.Comment
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommentRepository : JpaRepository<Comment, Long>{
