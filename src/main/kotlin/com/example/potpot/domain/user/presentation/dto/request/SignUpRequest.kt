85263c9a09f5851faebea1cd5d1ca30ebbe9eff2
eeuunn <haspring@dsm.hs.kr>
2024-11-15 15:48:32 +0900

    fix(user) : json 으로 요청 오게 설정

--- src/main/kotlin/com/example/potpot/domain/user/presentation/dto/request/SignUpRequest.kt
+++ src/main/kotlin/com/example/potpot/domain/user/presentation/dto/request/SignUpRequest.kt
@@ -1,14 +1,27 @@
 package com.example.potpot.domain.user.presentation.dto.request
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.NotNull
+
 data class SignUpRequest(
 
+    @JsonProperty("school_number")
+    @NotNull
     val schoolNumber: String,
 
+    @JsonProperty("name")
+    @NotNull
     val name: String,
 
+    @JsonProperty("password")
+    @NotNull
     val password: String,
 
+    @JsonProperty
+    @NotNull
     val email: String,
 
+    @JsonProperty("image_url")
+    @NotNull
     val imageUrl: String
 )
